ebe14e0487d77b1f8bff0ddb16c505db248d7c04
Mahadi Hasan <mahadi250721@gmail.com>
2021-08-03 14:03:16 +0600

    modified

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
    // Store Random Number
    const randomNum = Math.ceil(Math.random() * 100);
+   
    // for counting attempt initialize a value
-   var countGuessAttempt = 1;
    console.log(randomNum);
-   var btnClick = document.getElementById("submitButton");
+   let countGuessAttempt = 1;
+   let btnClick = document.getElementById("submitButton");
+
+   let result = document.getElementById("result");
+
    btnClick.addEventListener("click", function() {
-       var inputTextNum = document.getElementById("guessingNumber").value;
-       var inputTextNumConvert = parseFloat(inputTextNum);
-       alert(inputTextNumConvert);
+
+       if(countGuessAttempt >= 6){    //as question requirement. 6 tries the most.
+        document.getElementById("submitButton").style.visibility = 'hidden';
+        alert("No more attempt left.")
+       }
+
+       let inputTextNum = document.getElementById("guessingNumber").value;
+       let inputTextNumConvert = parseFloat(inputTextNum);
+
 
        if (randomNum == inputTextNumConvert) {
            alert("You got it right in " + countGuessAttempt + " tries!");
@@ -15,17 +25,31 @@
            document.getElementById("equal-num").innerText = inputTextNumConvert;
 
        } else if (randomNum < inputTextNumConvert) {
-           alert("Wrong! Too Long");
+
+           if ((inputTextNumConvert - randomNum) <=25){   //if differnce between them is less than 25 than print little high
+            result.innerHTML = "Wrong! Little High";
+           }
+           else{
+            result.innerHTML = "Wrong! Too High";
+           }
+
            countGuessAttempt++;
            document.getElementById("high-num").innerText = inputTextNumConvert;
            inputTextNum = document.getElementById("guessingNumber").value = "";
 
        } else if (randomNum > inputTextNumConvert) {
-           alert("Wrong! Low");
+
+        if ((randomNum - inputTextNumConvert) <=25){   //if differnce between them is less than 25 than print little low
+            result.innerHTML = "Wrong! Little Low";
+           }
+           else{
+            result.innerHTML = "Wrong! Too Low";
+           }
+
            countGuessAttempt++;
            document.getElementById("low-num").innerText = inputTextNumConvert;
            iinputTextNum = document.getElementById("guessingNumber").value = "";
 
-       }
+        }
 
    });
